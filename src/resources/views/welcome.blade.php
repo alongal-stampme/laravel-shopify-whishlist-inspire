05b314c2bd52e3fbbe01238ab2134416630c6947
Alon Gal <alon@stampme.com>
2020-08-03 15:24:05 +1000

    wip

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -82,7 +82,6 @@
                 <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Calendar</a>
             </div>
         </div>
-        Hello
     </nav>
 @endsection
 
