cbec0f30d3ffcda65a2f67c60abe472dc0637502
Alon Gal <alon@stampme.com>
2020-08-05 20:08:48 +1000

    06 - Laravel & Tailwind CSS Dashboard Customised for Shopify app

--- src/resources/views/vendor/shopify-app/layouts/default.blade.php
+++ src/resources/views/vendor/shopify-app/layouts/default.blade.php
@@ -6,7 +6,7 @@
 
         <title>{{ config('shopify-app.app_name') }}</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
+        <script src="https://unpkg.com/turbolinks"></script>
         @yield('styles')
     </head>
 
@@ -14,6 +14,7 @@
         <div class="app-wrapper">
             <div class="app-content">
                 <main role="main">
+                    @include('partials.navbar')
                     @yield('content')
                 </main>
             </div>
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 })->middleware(['auth.shopify'])->name('home');
+
+Route::get('/products', function () {
+    return view('products');
+})->middleware(['auth.shopify'])->name('products');
+
+Route::get('/customers', function () {
+    return view('customers');
+})->middleware(['auth.shopify'])->name('customers');
+
+Route::get('/settings', function () {
+    return view('settings');
+})->middleware(['auth.shopify'])->name('settings');
