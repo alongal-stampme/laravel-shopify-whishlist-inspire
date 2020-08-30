3ada28ae727328ccf42a6def0552aa99dd16cc5f
alongal-stampme <alon@stampme.com>
2020-08-30 21:03:46 +1000

    16 - Notification for Wishlist App in Laravel

--- src/resources/js/codeinspire-app.js
+++ src/resources/js/codeinspire-app.js
@@ -1,24 +1,42 @@
 import "../css/custom.css"
 
-function addWishlist() {
-    console.log('adding item to whislist')
+require("noty/src/noty.scss");
+require("noty/src/themes/mint.scss");
+
+window.Noty = require('noty')
+
+function addWishlist(customer, productId) {
+    new Noty({
+        type: 'success',
+        layout: 'topRight',
+        text: 'Added to wishlist',
+        timeout: 3000,
+    }).show();
+
+    // ajax request to the server
 }
 
 function removeWishlist() {
-    console.log("remove from wishlist")
-
-    // send https request
+    new Noty({
+        type: 'warning',
+        layout: 'topRight',
+        text: 'Removed from wishlist',
+        timeout: 3000,
+    }).show();
 }
 
 var wishlistButton = document.querySelector('.codeinspire-wishlist-btn')
 
-wishlistButton.addEventListener('click', function() {
+wishlistButton.addEventListener('click', function () {
 
     if (this.classList.contains('active')) {
         removeWishlist()
         this.classList.remove('active')
     } else {
         this.classList.add('active')
-        addWishlist()
+        let customer = this.dataset.customer
+        let id = this.dataset.product
+        // console.log('This: ', this.dataset.product)
+        addWishlist(customer, id)
     }
 })
